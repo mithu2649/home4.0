ebebb68f87d5ff92939f674202988b99802908d8
Abhishek Pandey <abhishekap2001@gmail.com>
2020-02-15 22:08:00 +0530

    Removed unnecessary id, style and event handler atrributes.

--- switches.php
+++ switches.php
@@ -11,51 +11,51 @@ require_once "header.php";
     <h2>Room <span style="color:#0099ff;margin:0;">1 <i class="fas fa-bed"></i></span></h2>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv1">
-            <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb 1 <sup id="active1"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb 1 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg1">.</div>
-        <div class="power-button" id="switchbtn1" status="off" onclick="switch1()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn1"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv2">
-            <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb 2 <sup id="active2"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb 2 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg2">.</div>
-        <div class="power-button" id="switchbtn2" status="off" onclick="switch2()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn2"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv3">
-            <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan 1 <sup id="active3"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan 1 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg3">.</div>
-        <div class="power-button" id="switchbtn3" status="off" onclick="switch3()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn3"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv4">
-            <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan 2 <sup id="active4"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan 2 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg4">.</div>
-        <div class="power-button" id="switchbtn4" status="off" onclick="switch4()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn4"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv5">
-            <div class="plans-text-switches"><i class="fas fa-plug" style="transform: rotate(30deg);"> </i> Plug 1 <sup id="active5"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fas fa-plug"> </i> Plug 1 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg5">.</div>
-        <div class="power-button" id="switchbtn5" status="off" onclick="switch5()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn5"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv6">
-            <div class="plans-text-switches"><i class="fas fa-plug" style="transform: rotate(30deg);"> </i> Plug 2 <sup id="active6"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fas fa-plug"> </i> Plug 2 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg6">.</div>
-        <div class="power-button" id="switchbtn6" status="off" onclick="switch6()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn6"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button"status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
     <!--   -----------------  ROOM 2     -->
     <!--   -----------------  ROOM 2     -->
@@ -67,51 +67,51 @@ require_once "header.php";
     <h2>Room <span style="color:#0099ff;margin:0;">2 <i class="fas fa-bed"></i></span></h2>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv7">
-            <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb 1 <sup id="active7"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb 1 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg7">.</div>
-        <div class="power-button" id="switchbtn7" status="off" onclick="switch7()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn7"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv8">
-            <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb 2 <sup id="active8"> Ofkff </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb 2 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg8">.</div>
-        <div class="power-button" id="switchbtn8" status="off" onclick="switch8()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn8"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv9">
-            <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan 1 <sup id="active9"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan 1 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg9">.</div>
-        <div class="power-button" id="switchbtn9" status="off" onclick="switch9()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn9"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv10">
-            <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan 2 <sup id="active10"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan 2 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg10">.</div>
-        <div class="power-button" id="switchbtn10" status="off" onclick="switch10()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn10"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv11">
-            <div class="plans-text-switches"><i class="fas fa-plug" style="transform: rotate(30deg);"> </i> Plug 1 <sup id="active11"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fas fa-plug"> </i> Plug 1 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg11">.</div>
-        <div class="power-button" id="switchbtn11" status="off" onclick="switch11()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn11"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv11">
-            <div class="plans-text-switches"><i class="fas fa-plug" style="transform: rotate(30deg);"> </i> Plug 2 <sup id="active11"> Off </sup></div>
+        <div class="planTextedit">
+            <div class="plans-text-switches"><i class="fas fa-plug"> </i> Plug 2 <sup> Off </sup></div>
         </div>
-        <div class="bridge-div" id="brg11">.</div>
-        <div class="power-button" id="switchbtn11" status="off" onclick="switch11()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn11"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
     </div>
 
 
@@ -127,35 +127,35 @@ require_once "header.php";
         <h2>Hallroom <span style="color:#0099ff;margin:0;"><i class="fas fa-couch"></i> </span></h2>
 
         <div class="plan-div">
-            <div class="planTextedit" id="planDiv13">
-                <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb <sup id="active13"> Off </sup></div>
+            <div class="planTextedit">
+                <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb <sup> Off </sup></div>
             </div>
-            <div class="bridge-div" id="brg13">.</div>
-            <div class="power-button" id="switchbtn13" status="off" onclick="switch13()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn13"></i></div>
+            <div class="bridge-div">.</div>
+            <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
         </div>
 
         <div class="plan-div">
-            <div class="planTextedit" id="planDiv14">
-                <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan <sup id="active14"> Off </sup></div>
+            <div class="planTextedit">
+                <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Fan <sup> Off </sup></div>
             </div>
-            <div class="bridge-div" id="brg14">.</div>
-            <div class="power-button" id="switchbtn14" status="off" onclick="switch14()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn14"></i></div>
+            <div class="bridge-div">.</div>
+            <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
         </div>
 
         <div class="plan-div">
-            <div class="planTextedit" id="planDiv15">
-                <div class="plans-text-switches"><i class="fas fa-fan fa-tv"></i> Television <sup id="active15"> Off </sup></div>
+            <div class="planTextedit">
+                <div class="plans-text-switches"><i class="fas fa-fan fa-tv"></i> Television <sup> Off </sup></div>
             </div>
-            <div class="bridge-div" id="brg15">.</div>
-            <div class="power-button" id="switchbtn15" status="off" onclick="switch15()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn15"></i></div>
+            <div class="bridge-div">.</div>
+            <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
         </div>
 
         <div class="plan-div">
-            <div class="planTextedit" id="planDiv16">
-                <div class="plans-text-switches"><i class="fas fa-plug" style="transform: rotate(30deg);"> </i> Plug <sup id="active16"> Off </sup></div>
+            <div class="planTextedit">
+                <div class="plans-text-switches"><i class="fas fa-plug"> </i> Plug <sup> Off </sup></div>
             </div>
-            <div class="bridge-div" id="brg16">.</div>
-            <div class="power-button" id="switchbtn16" status="off" onclick="switch16()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn16"></i></div>
+            <div class="bridge-div">.</div>
+            <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
         </div>
 
 
@@ -171,20 +171,20 @@ require_once "header.php";
         <h2>Kitchen <span style="color:#0099ff;margin:0;"> <i class="fas fa-utensils"></i></span></h2>
 
         <div class="plan-div">
-            <div class="planTextedit" id="planDiv17">
-                <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb <sup id="active17"> Off </sup></div>
+            <div class="planTextedit">
+                <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb <sup> Off </sup></div>
             </div>
-            <div class="bridge-div" id="brg17">.</div>
-            <div class="power-button" id="switchbtn17" status="off" onclick="switch17()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn17"></i></div>
+            <div class="bridge-div">.</div>
+            <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
         </div>
 
 
         <div class="plan-div">
-            <div class="planTextedit" id="planDiv18">
-                <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Exhaust Fan <sup id="active18"> Off </sup></div>
+            <div class="planTextedit">
+                <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Exhaust Fan <sup> Off </sup></div>
             </div>
-            <div class="bridge-div" id="brg18">.</div>
-            <div class="power-button" id="switchbtn18" status="off" onclick="switch18()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn18"></i></div>
+            <div class="bridge-div">.</div>
+            <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
         </div>
 
 
@@ -202,18 +202,18 @@ require_once "header.php";
         <h2>Bathroom <span style="color:#0099ff;margin:0;"> <i class="fas fa-bath"></i></span></h2>
 
         <div class="plan-div">
-            <div class="planTextedit" id="planDiv19">
-                <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb <sup id="active19"> Off </sup></div>
+            <div class="planTextedit">
+                <div class="plans-text-switches"><i class="fa fa-lightbulb-o"></i> Light Bulb <sup> Off </sup></div>
             </div>
-            <div class="bridge-div" id="brg19">.</div>
-            <div class="power-button" id="switchbtn19" status="off" onclick="switch19()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn19"></i></div>
+            <div class="bridge-div">.</div>
+            <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
         </div>
         <div class="plan-div">
-            <div class="planTextedit" id="planDiv20">
-                <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Exhaust Fan <sup id="active20"> Off </sup></div>
+            <div class="planTextedit">
+                <div class="plans-text-switches"><i class="fas fa-fan fa-spin"></i> Exhaust Fan <sup> Off </sup></div>
             </div>
-            <div class="bridge-div" id="brg20">.</div>
-            <div class="power-button" id="switchbtn20" status="off" onclick="switch20()"><i class="fa fa-bolt fa-3x" style="color: #ccc" id="btn20"></i></div>
+            <div class="bridge-div">.</div>
+            <div class="power-button" status="off"><i class="fa fa-bolt fa-3x"></i></div>
         </div>
 </div>
     <style>
--- users.php
+++ users.php
@@ -9,113 +9,113 @@ require_once "header.php";
 
     <div class="plan-div">
         <!-- Ahhishek -->
-        <div class="planTextedit" id="planDiv1">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
             <div class="img-profile" style="background-image: url(img/user-img/abh.jpg)"></div>
-            <div class="plans-text">Abhishek <sup id="active1"> Online </sup></div>
+            <div class="plans-text">Abhishek <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg1">.</div>
-        <div class="power-button" id="powerbtn1" status="off" onclick="block1()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn1"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
         <!-- It's me Debjeet! You Bantos -->
-        <div class="planTextedit" id="planDiv2">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a>
             </div>
             <div class="img-profile"></div>
-            <div class="plans-text">Debjeet <sup id="active2"> Online </sup></div>
+            <div class="plans-text">Debjeet <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg2">.</div>
-        <div class="power-button" id="powerbtn2" status="off" onclick="block2()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn2"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
         <!-- Rahul -->
-        <div class="planTextedit" id="planDiv3">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
             <div class="img-profile" style="background-image: url(img/user-img/rahul.jpg)"></div>
-            <div class="plans-text">Rahul <sup id="active3"> Online </sup></div>
+            <div class="plans-text">Rahul <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg3">.</div>
-        <div class="power-button" id="powerbtn3" status="off" onclick="block3()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn3"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
         <!-- Anushka -->
-        <div class="planTextedit" id="planDiv4">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
             <div class="img-profile" style="background-image: url(img/user-img/anu3.jpg)"></div>
-            <div class="plans-text">Anushka <sup id="active4"> Online </sup></div>
+            <div class="plans-text">Anushka <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg4">.</div>
-        <div class="power-button" id="powerbtn4" status="off" onclick="block4()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn4"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
         <!-- Shubhajit -->
-        <div class="planTextedit" id="planDiv5">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
             <div class="img-profile" style="background-image: url(img/user-img/shu.jpg)"></div>
-            <div class="plans-text">Shubhajit <sup id="active5"> Online </sup></div>
+            <div class="plans-text">Shubhajit <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg5">.</div>
-        <div class="power-button" id="powerbtn5" status="off" onclick="block5()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn5"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
         <!-- Koyel -->
-        <div class="planTextedit" id="planDiv6">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
             <div class="img-profile" style="background-image: url(img/user-img/k2.jpg)"></div>
-            <div class="plans-text">Koyel <sup id="active6"> Online </sup></div>
+            <div class="plans-text">Koyel <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg6">.</div>
-        <div class="power-button" id="powerbtn6" status="off" onclick="block6()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn6"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
         <!-- Souvik Da -->
-        <div class="planTextedit" id="planDiv7">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
             <div class="img-profile" style="background-image: url(img/user-img/souvik.jpg)"></div>
-            <div class="plans-text">Souvik <sup id="active7"> Online </sup></div>
+            <div class="plans-text">Souvik <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg7">.</div>
-        <div class="power-button" id="powerbtn7" status="off" onclick="block7()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn7"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
         <!-- Piu -->
-        <div class="planTextedit" id="planDiv8">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
             <div class="img-profile" style="background-image: url(img/user-img/piu3.jpg)"></div>
-            <div class="plans-text">Piu <sup id="active8"> Online </sup></div>
+            <div class="plans-text">Piu <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg8">.</div>
-        <div class="power-button" id="powerbtn8" status="off" onclick="block8()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn8"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
         <!-- guest 1 -->
-        <div class="planTextedit" id="planDiv9">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
             <div class="img-profile" style="background-image: url(img/user-img/guest1.png)"></div>
-            <div class="plans-text">Guest 1 <sup id="active9"> Online </sup></div>
+            <div class="plans-text">Guest 1 <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg9">.</div>
-        <div class="power-button" id="powerbtn9" status="off" onclick="block9()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn9"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
         <!-- guest 2 -->
-        <div class="planTextedit" id="planDiv10">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-user?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
             <div class="img-profile" style="background-image: url(img/user-img/guest2.png)"></div>
-            <div class="plans-text">Guest 2 <sup id="active10"> Online </sup></div>
+            <div class="plans-text">Guest 2 <sup> Online </sup></div>
         </div>
-        <div class="bridge-div" id="brg10">.</div>
-        <div class="power-button" id="powerbtn10" status="off" onclick="block10()"><i class="fa fa-ban fa-3x" style="color: #ccc" id="btn10"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-ban fa-3x"></i></div>
     </div>
     <div>
         <a href="Edit-user?sec=add">
