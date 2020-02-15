50065247cc82e94cf00145359d5b335b6cf6915a
Abhishek Pandey <abhishekap2001@gmail.com>
2020-02-15 21:33:25 +0530

    Removed unnecesarry id and event handler attributes.

--- plans.php
+++ plans.php
@@ -8,48 +8,48 @@ require_once "header.php";
     <h1><span style="color:#0099ff;margin:0;">P</span>lans</h1>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv1">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-plan?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
-            <div class="plans-text"><i class="fas fa-sun"></i> Day Plan <sup id="active1"> UnActive </sup></div>
+            <div class="plans-text"><i class="fas fa-sun"></i> Day Plan <sup> UnActive </sup></div>
         </div>
-        <div class="bridge-div" id="brg1">.</div>
-        <div class="power-button" id="powerbtn1" status="off" ><i class="fa fa-power-off fa-3x"  id="btn1"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-power-off fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv2">
+        <div class="planTextedit">
             <div style="display: inline-block"><a href="Edit-plan?sec=edit"><button class="editBtn" type="submit">Edit</button></a></div>
-            <div class="plans-text"><i class="fa fa-moon-o"></i> Night Plan <sup id="active2"> UnActive </sup></div>
+            <div class="plans-text"><i class="fa fa-moon-o"></i> Night Plan <sup> UnActive </sup></div>
         </div>
-        <div class="bridge-div" id="brg2">.</div>
-        <div class="power-button" id="powerbtn2" status="disbaled" onclick="glow2()"><i class="fa fa-power-off fa-3x"  id="btn2"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="disbaled"><i class="fa fa-power-off fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv3">
+        <div class="planTextedit">
             <div style="display: inline-block"><button class="editBtn" type="submit">Edit</button></div>
-            <div class="plans-text"><i class="fa fa-users"></i> Guest Plan <sup id="active3"> UnActive </sup></div>
+            <div class="plans-text"><i class="fa fa-users"></i> Guest Plan <sup> UnActive </sup></div>
         </div>
-        <div class="bridge-div" id="brg3">.</div>
-        <div class="power-button" id="powerbtn3" status="off" onclick="glow3()"><i class="fa fa-power-off fa-3x"  id="btn3"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-power-off fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv4">
+        <div class="planTextedit">
             <div style="display: inline-block"><button class="editBtn" type="submit">Edit</button></div>
-            <div class="plans-text"><i class="fa fa-pencil-square-o"></i> Custom Plan <sup id="active4"> UnActive </sup></div>
+            <div class="plans-text"><i class="fa fa-pencil-square-o"></i> Custom Plan <sup> UnActive </sup></div>
         </div>
-        <div class="bridge-div" id="brg4">.</div>
-        <div class="power-button" id="powerbtn4" status="off" onclick="glow4()"><i class="fa fa-power-off fa-3x"  id="btn4"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-power-off fa-3x"></i></div>
     </div>
 
     <div class="plan-div">
-        <div class="planTextedit" id="planDiv5">
+        <div class="planTextedit">
             <div style="display: inline-block"><button class="editBtn" type="submit">Edit</button></div>
-            <div class="plans-text"><i class="fas fa-door-open"></i> Outside Plan <sup id="active5"> UnActive </sup></div>
+            <div class="plans-text"><i class="fas fa-door-open"></i> Outside Plan <sup> UnActive </sup></div>
         </div>
-        <div class="bridge-div" id="brg5">.</div>
-        <div class="power-button" id="powerbtn5" status="off" onclick="glow5()"><i class="fa fa-power-off fa-3x"  id="btn5"></i></div>
+        <div class="bridge-div">.</div>
+        <div class="power-button" status="off"><i class="fa fa-power-off fa-3x"></i></div>
     </div>
     <div>
         <a href="Edit-plan?sec=add">
