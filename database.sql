93c67150fa6815671584d2374cf446e582cf821f
mithu2649 <mithu2649@gmail.com>
2020-03-07 02:19:49 +0530

    Removes SQL insert commands from SQL file

--- database.sql
+++ database.sql
@@ -49,28 +49,6 @@ CREATE TABLE `appliances` (
   `icon` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `appliances`
---
-
-INSERT INTO `appliances` (`id`, `app_name`, `room_id`, `status`, `icon`) VALUES
-(1, 'Light Bulb 1', 1, 'on', 'fa-lightbulb-o'),
-(2, 'Light Bulb 2', 1, 'on', 'fa-lightbulb-o'),
-(3, 'Fan', 1, 'off', 'fa-fan'),
-(4, 'Plug', 1, 'off', 'fa-plug'),
-(5, 'Light Bulb 1', 2, 'off', 'fa-lightbulb-o'),
-(6, 'Light Bulb 2', 2, 'off', 'fa-lightbulb-o'),
-(7, 'Fan', 2, 'off', 'fa-fan'),
-(8, 'Plug', 2, 'off', 'fa-plug'),
-(9, 'Light Bulb', 3, 'off', 'fa-lightbulb-o'),
-(10, 'Fan', 3, 'off', 'fa-fan'),
-(11, 'Television', 3, 'off', 'fa-television'),
-(12, 'Plug', 3, 'off', 'fa-plug'),
-(13, 'Light Bulb', 4, 'off', 'fa-lightbulb-o'),
-(14, 'Exhaust Fan', 4, 'off', 'fa-afn'),
-(15, 'Light Bulb', 5, 'off', 'fa-lightbulb-o'),
-(16, 'Exhaust Fan', 5, 'off', 'fa-fan');
-
 -- --------------------------------------------------------
 
 --
@@ -96,16 +74,6 @@ CREATE TABLE `rooms` (
   `icon` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `rooms`
---
-
-INSERT INTO `rooms` (`id`, `room_name`, `icon`) VALUES
-(1, 'Room 1', 'fa-bed'),
-(2, 'Room 2', 'fa-bed'),
-(3, 'Hall Room', 'fa-couch'),
-(4, 'Kitchen', 'fa-utensils'),
-(5, 'Bathroom', 'fa-bath');
 
 -- --------------------------------------------------------
 
@@ -126,15 +94,7 @@ CREATE TABLE `users` (
   `last_loggedin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`, `device_id`, `app_allowed`, `plan_allowed`, `status`, `last_loggedin`) VALUES
-(1, 'pandey', 'abhishekap2001@gmail.com', 'f362bd6883b7f4d5d02efcebd874900f6994f39c', 'admin', '44b0a734a29f8e2d887775662425bee2031bced2', '', '', '', '2020-02-23 09:13:36'),
-(2, 'debjeet', 'debjeet194@gmail.com', 'f362bd6883b7f4d5d02efcebd874900f6994f39c', 'admin', 'dd10b4cf73792bdadbe4214b63c540f0a14ed5ff', '', '', '', '2020-02-23 09:17:20');
-
---
 -- Indexes for dumped tables
 --
 
